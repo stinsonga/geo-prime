6e8c8591d0be219c641e4257b855bf5723bea39f
George Stinson <stinsonga@gmail.com>
2021-01-23 16:06:07 -0500

    Fixed quotes

--- geogh.py
+++ geogh.py
@@ -5,7 +5,7 @@ from random import choice, randint
 import string
 
 # Path to your intended repo
-github_path = "" 
+github_path = "https://github.com/stinsonga/GeoGH.git" 
 # Name of your textfile in which to store generated text
 filename = "blahwork.txt"
 commit_message = "Exciting, productive stuff "
@@ -16,8 +16,9 @@ def generate_stuff(length = 32, characters = string.ascii_letters + string.digit
 while True:
     for i in range(randint(1, 32)):
         stuff = generate_stuff(32, string.digits) + generate_stuff(32, string.ascii_letters)
-        os.system('echo "'+ stuff + '" >> "' + filename + "')
+        os.system('echo "'+ stuff + '" >> "' + filename + '")
     os.system('git add ' + filename)
     os.system('git commit -m' + commit_message + randint(1, 100))
+    os.system('git push origin master')
     time.sleep(60)
 
