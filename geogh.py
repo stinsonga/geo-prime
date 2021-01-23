785873c806c25ac4af0042b55040c754266f7edf
George Stinson <stinsonga@gmail.com>
2021-01-23 16:01:38 -0500

    Initial commit

---
+++ geogh.py
@@ -0,0 +1,23 @@
+
+import os
+import time
+from random import choice, randint
+import string
+
+# Path to your intended repo
+github_path = "" 
+# Name of your textfile in which to store generated text
+filename = "blahwork.txt"
+commit_message = "Exciting, productive stuff "
+
+def generate_stuff(length = 32, characters = string.ascii_letters + string.digits):
+    return ''.join([choice(characters) for i in range(length)])
+
+while True:
+    for i in range(randint(1, 32)):
+        stuff = generate_stuff(32, string.digits) + generate_stuff(32, string.ascii_letters)
+        os.system('echo "'+ stuff + '" >> "' + filename + "')
+    os.system('git add ' + filename)
+    os.system('git commit -m' + commit_message + randint(1, 100))
+    time.sleep(60)
+
